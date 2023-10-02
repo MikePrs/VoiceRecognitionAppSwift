0426d9cd6e2986d9aaab65d0f9d20ad0f15be3c1
Mike Paraskevopoulos <mikeparaskevopoulos.mp@gmail.com>
2023-10-02 17:19:47 +0300

    Initial Commit

---
+++ VoiceRecognitionApp/VoiceRecognitionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceRecognitionAppApp.swift
+//  VoiceRecognitionApp
+//
+//  Created by Mike Paraskevopoulos on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct VoiceRecognitionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
