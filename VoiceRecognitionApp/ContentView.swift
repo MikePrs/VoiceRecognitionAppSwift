28a7726dd50c7dc3d99bf70bb3c7a1dd1b8debcb
Mike Paraskevopoulos <mikeparaskevopoulos.mp@gmail.com>
2023-10-02 20:33:43 +0300

    speech to text + dynamic button + actual Text

--- VoiceRecognitionApp/ContentView.swift
+++ VoiceRecognitionApp/ContentView.swift
@@ -6,18 +6,54 @@
 //
 
 import SwiftUI
+import AVFoundation
+import Speech
 
 struct ContentView: View {
+    
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State private var transcript = ""
+    @State var isRecording = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+            VStack{
+                Image(systemName: isRecording ? "pause.circle.fill" :"mic.fill")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Button {
+                    if isRecording {
+                        stopRec()
+                    }else{
+                        startRec()
+                    }
+                } label: {
+                    Text(isRecording ?"Stop":"Start")
+                }
+            }
+            
+            if let text = speechRecognizer.transcript {
+                Text(text).frame(alignment: .center)
+            }
+            
+
+        }.onAppear{}
         .padding()
     }
+    
+    func startRec (){
+        isRecording.toggle()
+        speechRecognizer.resetTranscript()
+        speechRecognizer.startTranscribing()
+    }
+    
+    func stopRec() {
+        print(speechRecognizer.transcript)
+        speechRecognizer.stopTranscribing()
+        isRecording.toggle()
+    }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
